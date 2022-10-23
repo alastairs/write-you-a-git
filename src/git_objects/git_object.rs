267abe13a90b217ae44604483540cb1a7995b612
Alastair Smith <github@alastairsmith.me.uk>
2022-10-23 21:33:50 +0100

    Fix implementation of kvlm_parse
    The usages of skip() needed to be factored back into the pointer
    calculations after the initial values had been determined. The errors in
    calculations were much easier to locate following the introduction of
    the tests!

--- src/git_objects/git_object.rs
+++ src/git_objects/git_object.rs
@@ -71,7 +71,7 @@ impl GitObjectData {
         dct: Option<BTreeMap<String, Vec<String>>>,
     ) -> Result<BTreeMap<String, Vec<String>>, FromUtf8Error> {
         let mut dict = match dct {
-            None => BTreeMap::<String, Vec<String>>::new(),
+            None => BTreeMap::new(),
             Some(d) => d,
         };
 
@@ -81,27 +81,27 @@ impl GitObjectData {
         };
 
         let GitObjectData(_, data_vec) = self;
-        let raw: Vec<u8> = data_vec.iter().skip(start).cloned().collect();
-        let spc = raw.iter().position(|b| b == &b' ');
-        let nl = raw.iter().position(|b| b == &b'\n');
+        let line: Vec<u8> = data_vec.iter().skip(start).cloned().collect();
+        let mut _spc = line.iter().position(|b| b == &b' ');
+        let spc = *_spc.get_or_insert(start) + start;
+        let mut _nl = line.iter().position(|b| b == &b'\n');
+        let nl = *_nl.get_or_insert(start) + start;
 
-        // If space appears before a newline, we have a keyword.
+        // If space appears before a newline, we have a keyword and there's
+        // still more to parse.
 
         // Base case
         // ---------
         //
         // If newline appears first (or there's no space at all, in which case position() returns None), we
         // assume a blank line. A blank line means the remainder of the data is the message.
-        let base_case = match (spc, nl) {
-            (Some(s), Some(n)) => n < s,
-            _ => false,
-        };
+        let parsing_message = nl < spc;
 
-        if base_case {
-            assert!(Some(spc) == Some(nl));
+        if parsing_message {
+            assert!(nl == start);
             dict.insert(
                 String::from(""),
-                vec![String::from_utf8(raw[start + 1..].to_vec())?],
+                vec![String::from_utf8(data_vec[start + 1..].to_vec())?],
             );
 
             return Ok(dict);
@@ -111,25 +111,33 @@ impl GitObjectData {
         // --------------
         //
         // We read a key-value pair and recurse for the next
-        let key = String::from_utf8(raw[start + 1..].to_vec())?;
+        let key = String::from_utf8(data_vec[start..spc].to_vec())?;
 
         // Find the end of the value. Continuation lines begin with a space, so we loop until we find a "\n"
         // not followed by a space.
         let mut end = start;
+
         loop {
-            end = raw.iter().skip(end + 1).position(|b| b == &b'\n').unwrap();
-            if raw[end + 1] != b' ' {
+            end = data_vec
+                .iter()
+                .skip(end + 1)
+                .position(|b| b == &b'\n')
+                .unwrap()
+                + start
+                + 1;
+
+            if data_vec[end + 1] != b' ' {
                 break;
             }
         }
 
         // Grab the value
         // Also, drop the leading space on continuation lines
-        let value = String::from_utf8(raw[spc.unwrap() + 1..end].to_vec())?.replace("\n ", "\n");
+        let value = String::from_utf8(data_vec[spc + 1..end].to_vec())?.replace("\n ", "\n");
 
         // Don't overwrite existing data contents
         dict.entry(key)
-            .and_modify(|v| v.extend_from_slice(&[v[0].clone(), value.clone()]))
+            .and_modify(|v| v.extend_from_slice(&[value.clone()]))
             .or_insert(vec![value.clone()]);
 
         return self.kvlm_parse(Some(end + 1), Some(dict));
@@ -171,4 +179,117 @@ pub(crate) trait GitSerDe {
 
     /// Obtain the wrapped Repository object
     fn get_repo(&self) -> &Repository;
+
+    fn get_data(&self) -> GitObjectData {
+        return self.serialize();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const COMMIT_EXAMPLE: &str = "tree 4f0ae14e719a707c91ad1885e38bd18e7d214629
+parent 5f350c20eb579d51d0a3b1fafc8fce3f26d9d61a
+author Alastair Smith <github@alastairsmith.me.uk> 1666471801 +0100
+committer Alastair Smith <github@alastairsmith.me.uk> 1666471801 +0100
+
+Implement Commit object type
+
+Well, this forced me to resolve the polymorphism of the GitObject type,
+which I think has now been handled via traits and Box. Let's see if wyag
+commit works...
+";
+
+    #[test]
+    fn test_can_parse_tree_key_value() -> Result<(), FromUtf8Error> {
+        let commit = COMMIT_EXAMPLE;
+        let commit_object = GitObjectData(
+            "commit".to_string(),
+            String::from(commit).as_bytes().to_vec(),
+        );
+
+        let parsed = commit_object.kvlm_parse(None, None)?;
+        assert_eq!(
+            "4f0ae14e719a707c91ad1885e38bd18e7d214629",
+            parsed.get("tree").expect("Tree is empty")[0],
+            "{:?}",
+            parsed.get("tree"),
+        );
+
+        return Ok(());
+    }
+
+    #[test]
+    fn test_can_parse_parent_key_value() -> Result<(), FromUtf8Error> {
+        let commit = COMMIT_EXAMPLE;
+        let commit_object = GitObjectData(
+            "commit".to_string(),
+            String::from(commit).as_bytes().to_vec(),
+        );
+
+        let parsed = commit_object.kvlm_parse(None, None)?;
+        assert_eq!(
+            "5f350c20eb579d51d0a3b1fafc8fce3f26d9d61a",
+            parsed.get("parent").expect("Parents is empty")[0]
+        );
+
+        return Ok(());
+    }
+
+    #[test]
+    fn test_can_parse_author_key_value() -> Result<(), FromUtf8Error> {
+        let commit = COMMIT_EXAMPLE;
+        let commit_object = GitObjectData(
+            "commit".to_string(),
+            String::from(commit).as_bytes().to_vec(),
+        );
+
+        let parsed = commit_object.kvlm_parse(None, None)?;
+        assert_eq!(
+            "Alastair Smith <github@alastairsmith.me.uk> 1666471801 +0100",
+            parsed.get("author").expect("Author is empty")[0]
+        );
+
+        return Ok(());
+    }
+
+    #[test]
+    fn test_can_parse_committer_key_value() -> Result<(), FromUtf8Error> {
+        let commit = COMMIT_EXAMPLE;
+        let commit_object = GitObjectData(
+            "commit".to_string(),
+            String::from(commit).as_bytes().to_vec(),
+        );
+
+        let parsed = commit_object.kvlm_parse(None, None)?;
+        assert_eq!(
+            "Alastair Smith <github@alastairsmith.me.uk> 1666471801 +0100",
+            parsed.get("committer").expect("Committer is empty")[0]
+        );
+
+        return Ok(());
+    }
+
+    #[test]
+    fn test_can_parse_commit_message() -> Result<(), FromUtf8Error> {
+        let commit = COMMIT_EXAMPLE;
+        let commit_object = GitObjectData(
+            "commit".to_string(),
+            String::from(commit).as_bytes().to_vec(),
+        );
+
+        let parsed = commit_object.kvlm_parse(None, None)?;
+        assert_eq!(
+            "Implement Commit object type
+
+Well, this forced me to resolve the polymorphism of the GitObject type,
+which I think has now been handled via traits and Box. Let's see if wyag
+commit works...
+",
+            parsed.get("").expect("Message is empty")[0]
+        );
+
+        return Ok(());
+    }
 }
